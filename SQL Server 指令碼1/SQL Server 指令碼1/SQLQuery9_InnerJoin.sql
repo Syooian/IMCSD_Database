e7e9b19f0f52d15d7dedc4d275c90e84c5d0419f
Syooian <msn0126@hotmail.com>
2025-05-26 14:03:25 +0800

    Inner Join第三種寫法

--- SQL Server 指令碼1/SQL Server 指令碼1/SQLQuery9_InnerJoin.sql
+++ SQL Server 指令碼1/SQL Server 指令碼1/SQLQuery9_InnerJoin.sql
@@ -16,3 +16,16 @@ select * from
 		on e.員工編號 = o.員工編號)
 		on t.類別編號 = p.類別編號)
 		on d.貨運公司編號 = o.送貨方式
+
+--Inner Join第三種寫法
+--自然合併法、隱含合併法
+select * from 
+	貨運公司 d,	產品類別 t, 員工 e, 供應商 s,	客戶 c,	產品資料 p, 訂貨明細 od, 訂貨主檔 o
+	where
+		od.訂單號碼 = o.訂單號碼 and
+		p.產品編號 = od.產品編號 and
+		c.客戶編號 = o.客戶編號 and
+		s.供應商編號 = p.供應商編號 and
+		e.員工編號 = o.員工編號 and
+		t.類別編號 = p.類別編號 and
+		d.貨運公司編號 = o.送貨方式
