732e125f4228a8777697cbb4a722c4fe419515c9
Syooian <msn0126@hotmail.com>
2025-07-03 16:27:17 +0800

    預存程序 檢查帳號

---
+++ SQL Server 指令碼1/My/SQLQuery32_SP_檢查帳號.sql
@@ -0,0 +1,18 @@
+﻿
+
+--create procedure CheckAccount
+alter procedure CheckAccount
+	@Account nvarchar(5)
+as 
+begin
+	--檢查該帳號是否存在
+	declare @AccExists nvarchar(5) = ''
+	select @AccExists = 客戶編號 from 北風.dbo.客戶 where 客戶編號 = @Account
+	
+	if @AccExists is null
+		print '該帳號不存在'
+	else
+		print '該帳號存在'
+end
+
+exec CheckAccount 'A88I'
