4381c3abde5162fb48893334235fa0987e5916b2
Syooian <msn0126@hotmail.com>
2025-07-03 13:58:28 +0800

    建立預存程序並呼叫

---
+++ SQL Server 指令碼1/My/SQLQuery31_SP.sql
@@ -0,0 +1,13 @@
+﻿--建立預存程序
+create procedure GetOrderListWithOtherData
+as
+begin
+	SELECT 訂貨主檔.訂單號碼, 訂貨明細.訂單號碼 
+	AS Expr1, 訂貨明細.數量, 訂貨明細.折扣, 訂貨主檔.訂單日期, 訂貨主檔.運費, 單價*訂貨明細.數量*(1-訂貨明細.折扣) as 小計
+	FROM 訂貨主檔 INNER JOIN 訂貨明細 
+	ON 訂貨主檔.訂單號碼 = 訂貨明細.訂單號碼
+end
+
+--執行預存程序
+execute GetOrderListWithOtherData
+exec GetOrderListWithOtherData --execute的縮寫
