8f63f5d4909c26484972c039fb30819ece53ca12
Syooian <msn0126@hotmail.com>
2025-08-08 10:54:55 +0800

    重新生成dbStudents(科系)的內容

--- SQL Server 指令碼1/資料插入/dbStudents (含科系).sql
+++ SQL Server 指令碼1/資料插入/dbStudents (含科系).sql
@@ -1,63 +1,136 @@
--- --------------------------------------------------------
--- 主機:                           .
--- 伺服器版本:                        Microsoft SQL Server 2022 (RTM-GDR) (KB5058712) - 16.0.1140.6
--- 伺服器作業系統:                      Windows 10 Pro 10.0 <X64> (Build 19045: )
--- HeidiSQL 版本:                  12.8.0.6908
--- --------------------------------------------------------
+USE [master]
+GO
+/****** Object:  Database [dbStudents]    Script Date: 2025/8/8 上午 10:40:03 ******/
+drop database [dbStudents]
+CREATE DATABASE  [dbStudents]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'dbStudents', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\dbStudents.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'dbStudents_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\dbStudents_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [dbStudents] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [dbStudents].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [dbStudents] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [dbStudents] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [dbStudents] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [dbStudents] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [dbStudents] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [dbStudents] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [dbStudents] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [dbStudents] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [dbStudents] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [dbStudents] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [dbStudents] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [dbStudents] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [dbStudents] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [dbStudents] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [dbStudents] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [dbStudents] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [dbStudents] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [dbStudents] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [dbStudents] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [dbStudents] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [dbStudents] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [dbStudents] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [dbStudents] SET RECOVERY FULL 
+GO
+ALTER DATABASE [dbStudents] SET  MULTI_USER 
+GO
+ALTER DATABASE [dbStudents] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [dbStudents] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [dbStudents] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [dbStudents] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [dbStudents] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [dbStudents] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'dbStudents', N'ON'
+GO
+ALTER DATABASE [dbStudents] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [dbStudents] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [dbStudents]
+GO
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES  */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- 傾印 dbStudents 的資料庫結構
-DROP DATABASE IF EXISTS "dbStudents";
-CREATE DATABASE IF NOT EXISTS "dbStudents";
-USE "dbStudents";
-
--- 傾印  資料表 dbStudents.Department 結構
-DROP TABLE IF EXISTS "Department";
-CREATE TABLE IF NOT EXISTS "Department" (
-	"DeptID" CHAR(2) NOT NULL COLLATE 'Chinese_Taiwan_Stroke_CI_AS',
-	"DeptName" NVARCHAR(10) NOT NULL COLLATE 'Chinese_Taiwan_Stroke_CI_AS',
-	PRIMARY KEY ("DeptID")
-);
-
--- 正在傾印表格  dbStudents.Department 的資料：-1 rows
-/*!40000 ALTER TABLE "Department" DISABLE KEYS */;
-INSERT INTO "Department" ("DeptID", "DeptName") VALUES
-	('01', '數學系'),
-	('02', '企管系'),
-	('03', '企管系');
-/*!40000 ALTER TABLE "Department" ENABLE KEYS */;
-
--- 傾印  資料表 dbStudents.tStudent 結構
-DROP TABLE IF EXISTS "tStudent";
-CREATE TABLE IF NOT EXISTS "tStudent" (
-	"fStuId" CHAR(6) NOT NULL COLLATE 'Chinese_Taiwan_Stroke_CI_AS',
-	"fName" NVARCHAR(30) NOT NULL COLLATE 'Chinese_Taiwan_Stroke_CI_AS',
-	"fEmail" NVARCHAR(40) NULL DEFAULT NULL COLLATE 'Chinese_Taiwan_Stroke_CI_AS',
-	"fScore" INT NULL DEFAULT NULL,
-	"DeptID" CHAR(2) NULL DEFAULT NULL COLLATE 'Chinese_Taiwan_Stroke_CI_AS',
-	PRIMARY KEY ("fStuId"),
-	FOREIGN KEY INDEX "tStudent_Department_FK" ("DeptID"),
-	CONSTRAINT "tStudent_Department_FK" FOREIGN KEY ("DeptID") REFERENCES "Department" ("DeptID") ON UPDATE NO_ACTION ON DELETE NO_ACTION
-);
-
--- 正在傾印表格  dbStudents.tStudent 的資料：-1 rows
-/*!40000 ALTER TABLE "tStudent" DISABLE KEYS */;
-INSERT INTO "tStudent" ("fStuId", "fName", "fEmail", "fScore", "DeptID") VALUES
-	('112001', '王普丁', 'ding@gmail.com', 92, '01'),
-	('112002', '習維尼', 'nee@gmail.com', 68, '02'),
-	('112003', '金小胖', 'kim@gmail.com', 88, '03');
-/*!40000 ALTER TABLE "tStudent" ENABLE KEYS */;
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/****** Object:  Table [dbo].[Department]    Script Date: 2025/8/8 上午 10:40:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Department](
+	[DeptID] [varchar](2) NOT NULL,
+	[DeptName] [nvarchar](30) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[DeptID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[tStudent]    Script Date: 2025/8/8 上午 10:40:03 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tStudent](
+	[fStuId] [char](6) NOT NULL,
+	[fName] [nvarchar](30) NOT NULL,
+	[fEmail] [nvarchar](40) NULL,
+	[fScore] [int] NULL,
+	[DeptID] [varchar](2) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[fStuId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+INSERT [dbo].[Department] ([DeptID], [DeptName]) VALUES (N'01', N'資工系')
+INSERT [dbo].[Department] ([DeptID], [DeptName]) VALUES (N'02', N'資管系')
+INSERT [dbo].[Department] ([DeptID], [DeptName]) VALUES (N'03', N'工管系')
+GO
+INSERT [dbo].[tStudent] ([fStuId], [fName], [fEmail], [fScore], [DeptID]) VALUES (N'112001', N'王普丁', N'ding@gmail.com', 65, N'01')
+INSERT [dbo].[tStudent] ([fStuId], [fName], [fEmail], [fScore], [DeptID]) VALUES (N'112002', N'習維尼', N'nee@gmail.com', 68, N'01')
+INSERT [dbo].[tStudent] ([fStuId], [fName], [fEmail], [fScore], [DeptID]) VALUES (N'112003', N'金小胖', N'kim@gmail.com', 88, N'01')
+GO
+ALTER TABLE [dbo].[tStudent] ADD  DEFAULT ('01') FOR [DeptID]
+GO
+ALTER TABLE [dbo].[tStudent]  WITH CHECK ADD FOREIGN KEY([DeptID])
+REFERENCES [dbo].[Department] ([DeptID])
+GO
+USE [master]
+GO
+ALTER DATABASE [dbStudents] SET  READ_WRITE 
+GO
